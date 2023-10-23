a3e82c63e1897110612d9e589effc10a00e8be9a
Julia Farias de Queiroz <133894616+juhfarias31@users.noreply.github.com>
2023-10-23 09:21:09 -0300

    Add files via upload

---
+++ PrjGame/PrjGame/target/classes/static/JavaScript.js
@@ -0,0 +1,45 @@
+document.getElementById('cadastroForm').addEventListener('submit', cadastrarJogo);
+function cadastrarJogo(event){
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const plataform = document.getElementById('plataform').value;
+    fetch('http://localhost:8080/jogos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name, plataform}),
+    })
+        .then(Response => Response.json())
+        .then(data => {
+            alert('Jogo cadastro com sucesso!');
+            document.getElementById('cadastroForm').reset();
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar jogo:', error);
+        });
+}
+
+function pesquisarJogo(){
+    const searchId = document.getElementById('searchId').value;
+    fetch(`http://localhost:8080/jogos/${searchId}`)
+        .then(Response => {
+            if(Response.status === 404){
+                return Promise.reject('Jogo não encontrado');
+            }
+            return Response.json();
+        })
+        .then(data => {
+            constresultadoPesquisa = document.getElementById('resultadoPesquisa');
+            resultadoPesquisa.innerHTML = `
+            <h3>ID: ${data.id}</h3>
+            <p>Nome: ${data.name}</p>
+            <p>Plataforma: ${data.plataform}</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Erro ao pesquisar jogo:',error);
+            const resultadoPesquisa = document.getElementById('resultadoPesquisa');
+            resultadoPesquisa.innerHTML = 'Jogo não encontrado.';
+        })
+}
